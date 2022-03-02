4912942c2dc168bd6b7e533c579c000684bba498
KhubaibAhmadw2 <khubaibahmadw2@gmail.com>
2022-03-02 15:47:48 +0500

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import memories from './images/memories.png';
+import { useDispatch } from 'react-redux';
+
 import Posts from './conponents/Posts/Posts';
 import Form from './conponents/Form/Form';
-import useStyles from './styles'
+import { getPosts } from './actions/posts';
+import useStyles from './styles';
+import memories from './images/memories.png';
 
-const App=()=> {
+const App = () => {
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
   const classes = useStyles();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
   return (
-    
-    <Container maxwidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography varaint="h2" align="center"> Memories </Typography>
-       
-        <img src={memories} width="100" height="50"/>
-         
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
-          <Grid cantainer justify ="space-between" align item ="stretch" spacing={3}>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
-               <Posts/>
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
-               <Form/>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
-
         </Container>
       </Grow>
     </Container>
-)
-}
+  );
+};
 
-export default App
+export default App;
