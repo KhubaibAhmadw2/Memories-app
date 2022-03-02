0b8533828817a6a425e48f5c40005d36c84fa069
KhubaibAhmadw2 <khubaibahmadw2@gmail.com>
2022-03-02 23:18:20 +0500

    first run

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/posts.js';
 
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 
+app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://Khubaib:4422@cluster0.bxr01.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
